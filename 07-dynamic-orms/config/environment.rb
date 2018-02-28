2f806d7ef4bc3eba9ed5a25c923c7940c09e1111
nikymorg <nicole.morgan@gmail.com>
2018-02-28 12:51:50 -0500

    dynamic orm starter code

---
+++ 07-dynamic-orms/config/environment.rb
@@ -0,0 +1,11 @@
+# load gems
+require 'pry'
+require 'sqlite3'
+
+# creating a database connection
+
+DB = SQLite3::Database.new('db/chinook.db')
+DB.results_as_hash = true
+
+require_relative '../app/models/fan.rb'
+# DB.execute('create table if not exists fans (id INTEGER PRIMARY KEY, name varchar(255), artist_id int)')
