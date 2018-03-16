64b5ac20e5b3f8ad0c588c64ea53adf751601e0d
howardbdev <howarddevennish@gmail.com>
2018-03-16 13:05:34 -0400

    add folder for intro to rails - more coming soon

---
+++ 15-intro-to-rails/dinosaurs/app/controllers/dinosaurs_controller.rb
@@ -0,0 +1,21 @@
+class DinosaursController < ApplicationController
+  def index
+    @dinosaurs = Dinosaur.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
