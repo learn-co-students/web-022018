dd964a9433a795463cbc27d0f1299200292334d2
howardbdev <howarddevennish@gmail.com>
2018-03-16 15:24:30 -0400

    update code with comments and new forms

--- 15-intro-to-rails/dinosaurs/app/controllers/dinosaurs_controller.rb
+++ 15-intro-to-rails/dinosaurs/app/controllers/dinosaurs_controller.rb
@@ -4,7 +4,7 @@ class DinosaursController < ApplicationController
   end
 
   def show
-
+    @dinosaur = Dinosaur.find_by(id: params[:id])
   end
 
   def edit
@@ -12,10 +12,24 @@ class DinosaursController < ApplicationController
   end
 
   def new
-
+    # if we want to use rails form builder methods like form_for,
+    # we have to provide an instance of the model we're building
+    @dinosaur = Dinosaur.new
   end
 
   def create
+    new_dino = Dinosaur.new(dino_params)
 
+    if new_dino.save
+      redirect_to dinosaur_path(new_dino)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dino_params
+    params.require(:dinosaur).permit(:name, :species, :number_of_teeth, :era, :diet)
   end
 end
--- 15-intro-to-rails/dinosaurs/db/schema.rb
+++ 15-intro-to-rails/dinosaurs/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316143852) do
+ActiveRecord::Schema.define(version: 20180316175459) do
+
+  create_table "cavemen", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dinosaurs", force: :cascade do |t|
     t.string "name"
